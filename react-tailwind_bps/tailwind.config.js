690dc3fa50ace25dec2adb9f571feec129737bba
Joaquin CR <j.rodriguez@blueparrotsoftwarellc.com>
2023-06-16 12:16:41 -0600

    Created the custom calsses

--- react-tailwind_bps/tailwind.config.js
+++ react-tailwind_bps/tailwind.config.js
@@ -8,12 +8,18 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        'widescreen': {
-          'raw': '(min-aspect-ratio: 3/2)',
-        },
-        'tallscreen': {
-          'raw': '(min-aspect-ratio: 1/2)',
+        'widescreen': { 'raw': '(min-aspect-ratio: 3/2)' },
+        'tallscreen': { 'raw': '(min-aspect-ratio: 13/20)' }
+      },
+      keyframes: {
+        'open-menu': {
+          '0%': { transform: 'scaleY(0)' },
+          '80%': { transform: 'scaleY(1.2)' },
+          '100%': { transform: 'scaleY(1)' }
         }
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5 ease-in-out fowards',
       },
       // backgroundImage: {
       //   'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
